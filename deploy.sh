23286341396a1252ed1e5f0d476006abbaa36c4a
Mazhar Choudhry <mazhar@objex.ca>
2020-12-28 16:01:25 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $# -lt 4 ]] ; then
-    echo 'Valid arguments: ./bg-deploy.sh serviceName serviceVersion 192.168.64.2:32000 deploymentName'
+    echo 'Valid arguments: ./deploy.sh serviceName serviceVersion 192.168.64.2:32000 k8DeploymentName'
     exit 0
 fi
 
@@ -15,9 +15,11 @@ IMAGENAME=${REGISTRYIP}/${DEPLOYMENTNAME}:"registry"
 echo "deploying " + ${DEPLOYMENTNAME} + "to " + ${REGISTRYIP}
 
 # Build a new Image
-./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=${IMAGENAME}
-echo "Image successfully buit, now deployting it to private registry"
-docker push ${REGISTRYIP}/${DEPLOYMENTNAME}
+# ./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=${IMAGENAME}
+docker load < microservice.tar
+docker tag objex/k8s-demo-app:latest  ${IMAGENAME}
+echo "Image successfully loaded and tagged, now deploying it to microk8s private registry"
+docker push ${IMAGENAME}
 curl ${REGISTRYIP}/v2/_catalog
 
 # Rollout new update
